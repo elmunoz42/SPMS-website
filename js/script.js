5d9a1dfb7be08ed39ac1af133fdc77ef59984ea4
Carlos Munoz Kampff and Jake Campa <carlos@>
2017-01-10 09:40:10 -0800

    toggle hover pix

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+  $(".hover-pix").hover(function() {
+    $("#index-bottom-pix2").toggle(),
+    $("#index-bottom-pix1").toggle();
+    // $("img").hide();
+    //$("#index-bottom-pix2").toggle();
+
+  });
+});
