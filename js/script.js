7544281b3cacc438d3a62d7f038a2169f1124409
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-01-11 13:47:03 -0800

    form submit populates email text (partially set up at the moment)

--- js/script.js
+++ js/script.js
@@ -6,24 +6,33 @@ $(function() {
     //$("#index-bottom-pix2").toggle();
 
   });
-
-  //Theme changer
-  $("#dark-theme-button").click(function() {
-    $("body").removeClass("toGrayTheme");
-    $("body").addClass("toDarkTheme");
-
-  });
-  $("#gray-theme-button").click(function() {
-    $("body").removeClass("toDarkTheme");
-    $("body").addClass("toGrayTheme");
-
-  });
-  $("#normal-theme-button").click(function() {
-    $("body").removeClass("toDarkTheme");
-    $("body").removeClass("toGrayTheme");
-  });
+  //
+  // //Theme changer
+  // $("#dark-theme-button").click(function() {
+  //   $("body").removeClass("toGrayTheme");
+  //   $("body").addClass("toDarkTheme");
+  //
+  // });
+  // $("#gray-theme-button").click(function() {
+  //   $("body").removeClass("toDarkTheme");
+  //   $("body").addClass("toGrayTheme");
+  //
+  // });
+  // $("#normal-theme-button").click(function() {
+  //   $("body").removeClass("toDarkTheme");
+  //   $("body").removeClass("toGrayTheme");
+  // });
 
   //Letter maker
-  
+  $("#intake-form-SPMS").submit(function(event) {
+    // $(".Family-Name").prepend(input);
+    var inputFN=$("input#FN").val();
+    var inputPG=$("input#PG").val();
+    var inputSN=$("input#SN").val()
+    $("p .Parent-Guardian-Name").text(inputPG);
+    $("p .Student-Name").text(inputSN);
+
+  event.preventDefault();
+  });
 
 });
